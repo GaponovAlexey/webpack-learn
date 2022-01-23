294feaef1f23c204cadae6ff37c263a1b92afee4
Kekelberi <alexei.gaponov@gmail.com>
2022-01-23 20:03:37 +0300

    save

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'development',// убирает минификацию
+  mode: 'development', // убирает минификацию
   entry: {
-    main: './src/index.js',// чанки 
+    main: './src/index.js', // чанки
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    filename: '[name].bundle.js', // что взять
+    path: path.resolve(__dirname, 'dist'), // куда положить
+  },
+  plugins: [new htmlWebpackPlugin()],
 }
